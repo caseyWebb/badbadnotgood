d1701fa8b2c061be7455bc1de84fe11f8f8f8068
Casey Webb <notcaseywebb@gmail.com>
2019-10-25 15:55:02 -0500

    test: add forEach.test-d.ts

---
+++ test/forEach.test-d.ts
@@ -0,0 +1,15 @@
+import { expectType } from 'tsd'
+import { forEach, equals, SyncValidator, ForeachValidatorResult } from '../src'
+
+expectType<SyncValidator<string[], ForeachValidatorResult<never>>>(
+  forEach(equals('foo'))
+)
+expectType<SyncValidator<string[], string | ForeachValidatorResult<never>>>(
+  forEach(equals('foo'), '')
+)
+expectType<SyncValidator<string[], ForeachValidatorResult<string>>>(
+  forEach(equals('foo', ''))
+)
+expectType<SyncValidator<string[], string | ForeachValidatorResult<string>>>(
+  forEach(equals('foo', ''), '')
+)
